5833d7681326d32378da08e3b30ea846d7758f23
deathcap <deathcap@gmx.co.uk>
2014-05-12 17:30:09 -0700

    Set generate() voxels using ndarray API instead of data array indexing

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ function generate(lo, hi, fn, game) {
   hi[2]++
   var dims = [hi[2]-lo[2], hi[1]-lo[1], hi[0]-lo[0]]
   var data = ndarray(new Uint16Array(dims[2] * dims[1] * dims[0]), dims)
-  for (var n = 0, k = lo[2]; k < hi[2]; k++)
+  for (var k = lo[2]; k < hi[2]; k++)
     for (var j = lo[1]; j < hi[1]; j++)
-      for(var i = lo[0]; i < hi[0]; i++, n++) {
-        data.data[n] = fn(i, j, k, n, game)
+      for(var i = lo[0]; i < hi[0]; i++) {
+        data.set(k-lo[2], j-lo[1], i-lo[0], fn(i, j, k))
       }
   return data
 }
